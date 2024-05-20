abdc2c1b4b93fdda1c0de670c3b3420665188e8f
YogDPatil <ydpatil.it@gmail.com>
2024-05-20 15:50:45 +0530

    take screenshot code is added in browser utils

--- src/test/java/com/ui/tests/LoginPageTest.java
+++ src/test/java/com/ui/tests/LoginPageTest.java
@@ -5,16 +5,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import com.constants.Browser;
 import com.ui.pages.LoginPage;
+import com.utils.BrowserUtils;
 
+@Listeners(com.listeners.UiListeners.class)
 public class LoginPageTest {
 	WebDriver driver;
 	LoginPage loginPage;
 	@BeforeMethod(description = "setup browser and load the page")
 	public void setup() {
 		System.setProperty("webdriver.driver.chrome", System.getProperty("user.dir")+"/drivers.chromedriver");
+		//BrowserUtils.initialiseDriver(Browser.CHROME);
 		driver = new ChromeDriver();
 		loginPage = new LoginPage(driver, "http://phoenix.testautomationacademy.in/sign-in");
 	}
@@ -22,7 +27,7 @@ public class LoginPageTest {
 	@Test(description = "test the user can login by ui")
 	public void testLoginByUi() {
 		
-		Assert.assertEquals(loginPage.doLogin("iamfd", "password").getUsername(), "iamfd");
+		Assert.assertEquals(loginPage.doLogin("iamfd", "password").getUsername(), "iamsup");
 		
 	}
 
